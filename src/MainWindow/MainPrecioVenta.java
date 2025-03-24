66c730000bf2b399b8795d1642f0ba661e4531eb
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-23 23:40:48 -0500

    Se añadio la estructura y configuracion de la ventana main en un package de logica llamada MainWindow y la interfaz esta en otro package llamado igu

---
+++ src/MainWindow/MainPrecioVenta.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MainWindow;
+
+import igu.PrecioVentaIGU;
+
+/**
+ *
+ * @author zDefcon
+ */
+public class MainPrecioVenta {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+      PrecioVentaIGU Window = new PrecioVentaIGU();
+      Window.setVisible(true);
+      Window.setResizable(false);
+      Window.setLocationRelativeTo(null);
+  }
+  
+}
